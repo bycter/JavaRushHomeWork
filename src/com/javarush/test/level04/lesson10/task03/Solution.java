9a04c783868ad6b182806e1fb2a8247f081096b2
Denis Yoshchyk <bycter@gmail.com>
2016-12-22 23:43:25 +0300

    level04lesson07task03-04 download level04lesson10task01-05 download level04lesson13task01-05 download

---
+++ src/com/javarush/test/level04/lesson10/task03/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level04.lesson10.task03;
+
+/* Хорошего не бывает много
+Ввести с клавиатуры строку и число N.
+Вывести на экран строку N раз используя цикл while.
+Пример ввода:
+абв
+2
+Пример вывода:
+абв
+абв
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+
+    }
+}
---
+++ src/com/javarush/test/level04/lesson13/task03/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level04.lesson13.task03;
+
+import java.io.*;
+
+/* Рисуем треугольник
+Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
+Пример:
+8
+88
+888
+...
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+
+    }
+}
