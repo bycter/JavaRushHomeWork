baebee085bc5ba1bf37972bcdfd14a54ad776313
Denis Yoshchyk <bycter@gmail.com>
2016-12-23 13:20:56 +0300

    level04lesson13task01 completed, not checked

--- src/com/javarush/test/level04/lesson13/task01/Solution.java
+++ src/com/javarush/test/level04/lesson13/task01/Solution.java
@@ -5,11 +5,17 @@ package com.javarush.test.level04.lesson13.task01;
 Через пробел либо с новой строки.
 */
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
-        //напишите тут ваш код
+public class Solution {
+    public static void main(String[] args) throws Exception {
 
+        int numberMin = 1;
+        int numberMax = 100;
+
+        for (int i = numberMin; i <= numberMax; i++) {
+            if ((i % 2) == 0) {
+                System.out.print(i);
+                System.out.print(" ");
+            }
+        }
     }
 }
