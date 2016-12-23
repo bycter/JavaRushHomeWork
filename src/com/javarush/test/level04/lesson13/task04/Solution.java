798230a645866bcb008574132b70373f42d2bf5c
Denis Yoshchyk <bycter@gmail.com>
2016-12-23 13:55:17 +0300

    level04lesson13task04 completed, not checked

--- src/com/javarush/test/level04/lesson13/task04/Solution.java
+++ src/com/javarush/test/level04/lesson13/task04/Solution.java
@@ -1,18 +1,23 @@
 package com.javarush.test.level04.lesson13.task04;
 
-import java.io.*;
-
 /* Рисуем линии
 Используя цикл for вывести на экран:
 - горизонтальную линию из 10 восьмёрок
 - вертикальную линию из 10 восьмёрок.
 */
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
-        //напишите тут ваш код
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int lengthGorLine = 10;
+        int heightVertLine = 10;
 
+        for (int i = 0; i < lengthGorLine; i++) {
+            System.out.print("8");
+        }
+        System.out.println("");
+
+        for (int j = 0; j < heightVertLine; j++) {
+            System.out.println("8");
+        }
     }
 }
