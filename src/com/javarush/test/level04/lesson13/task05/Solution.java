86b011dba82082fc3488259acaa8b7a89e2f81aa
Denis Yoshchyk <bycter@gmail.com>
2016-12-23 13:59:16 +0300

    level04lesson13task05 completed, not checked

--- src/com/javarush/test/level04/lesson13/task05/Solution.java
+++ src/com/javarush/test/level04/lesson13/task05/Solution.java
@@ -1,20 +1,23 @@
 package com.javarush.test.level04.lesson13.task05;
 
-import java.io.*;
-
-/* Все любят Мамбу
+import java.io.BufferedReader;/* Все любят Мамбу
 Ввести с клавиатуры имя и используя цикл for 10 раз вывести: [*имя* любит меня.]
 Пример текста:
 Света любит меня.
 Света любит меня.
 …
 */
+import java.io.InputStreamReader;
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
-        //напишите тут ваш код
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String s = reader.readLine();
+
+        int counter = 10;
+        for (int i = 0; i < counter; i++) {
+            System.out.println(s + " любит меня.");
+        }
     }
 }
