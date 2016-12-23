684fcfc15de9edcbe001d703a7aa443d686071f0
Denis Yoshchyk <bycter@gmail.com>
2016-12-23 12:48:22 +0300

    level04lesson10task02 completed, not check

--- src/com/javarush/test/level04/lesson10/task02/Solution.java
+++ src/com/javarush/test/level04/lesson10/task02/Solution.java
@@ -1,7 +1,5 @@
 package com.javarush.test.level04.lesson10.task02;
 
-import java.io.*;
-
 /* 10 чисел наоборот
 Вывести на экран числа от 10 до 1 используя цикл while.
 */
@@ -10,7 +8,13 @@ public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //напишите тут ваш код
+        int counter = 0;
+        int counterMax = 10;
+        while (counter < counterMax)
+        {
+            System.out.println(counterMax - counter);
+            counter++;
+        }
 
     }
 }
