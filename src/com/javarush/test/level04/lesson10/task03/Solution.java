cff8505678c1071540d2aa20451d02687ea26d75
Denis Yoshchyk <bycter@gmail.com>
2016-12-23 12:56:43 +0300

    level04lesson10task03 completed, not checked

--- src/com/javarush/test/level04/lesson10/task03/Solution.java
+++ src/com/javarush/test/level04/lesson10/task03/Solution.java
@@ -11,11 +11,23 @@ package com.javarush.test.level04.lesson10.task03;
 абв
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = reader.readLine();
+        int n = Integer.parseInt(reader.readLine());
+
+        int i = 0;
+        while (i < n){
+            System.out.println(s);
+            i++;
+        }
 
     }
 }
