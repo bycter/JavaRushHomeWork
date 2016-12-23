ed2073b711b0f8e49bd999eeabf84fef87fc1963
Denis Yoshchyk <bycter@gmail.com>
2016-12-23 12:46:39 +0300

    level04lesson10task01 completed, not check

--- src/com/javarush/test/level04/lesson10/task01/Solution.java
+++ src/com/javarush/test/level04/lesson10/task01/Solution.java
@@ -1,16 +1,18 @@
 package com.javarush.test.level04.lesson10.task01;
 
-import java.io.*;
-
 /* 10 чисел
 Вывести на экран числа от 1 до 10 используя цикл while.
 */
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
-        //напишите тут ваш код
+public class Solution {
+    public static void main(String[] args) throws Exception {
 
+        int counter = 0;
+        int counterMax = 10;
+        while (counter < counterMax)
+        {
+            System.out.println(counter + 1);
+            counter++;
+        }
     }
 }
