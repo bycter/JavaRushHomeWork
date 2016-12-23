2faab73508a2f2e80e8be7c0cc56a94fa3d169ed
Denis Yoshchyk <bycter@gmail.com>
2016-12-23 13:50:55 +0300

    level04lesson13task03 completed, not checked

--- src/com/javarush/test/level04/lesson13/task03/Solution.java
+++ src/com/javarush/test/level04/lesson13/task03/Solution.java
@@ -1,7 +1,5 @@
 package com.javarush.test.level04.lesson13.task03;
 
-import java.io.*;
-
 /* Рисуем треугольник
 Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
 Пример:
@@ -11,11 +9,21 @@ import java.io.*;
 ...
 */
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
-        //напишите тут ваш код
+public class Solution {
+    public static void main(String[] args) throws Exception {
 
+        int heightMax = 10;
+        int lengthMax = 10;
+
+        int length = 1;
+        for (int i = 0; i < heightMax; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print("8");
+            }
+            if (length <= lengthMax) {
+                length++;
+            }
+            System.out.println("");
+        }
     }
 }
