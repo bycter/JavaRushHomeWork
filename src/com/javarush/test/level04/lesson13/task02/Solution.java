454f6e1dae03470691e50d05f8af53f3a7c0ed55
Denis Yoshchyk <bycter@gmail.com>
2016-12-23 13:33:40 +0300

    level04lesson13task02 completed, not checked

--- src/com/javarush/test/level04/lesson13/task02/Solution.java
+++ src/com/javarush/test/level04/lesson13/task02/Solution.java
@@ -1,7 +1,5 @@
 package com.javarush.test.level04.lesson13.task02;
 
-import java.io.*;
-
 /* Рисуем прямоугольник
 Ввести с клавиатуры два числа m и n.
 Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.
@@ -10,11 +8,22 @@ import java.io.*;
 8888
 */
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
-        //напишите тут ваш код
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int heightMax = Integer.parseInt(reader.readLine());
+        int lengthMax = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < heightMax; i++) {
+            for (int j = 0; j < lengthMax; j++) {
+                System.out.print("8");
+            }
+            System.out.println("");
+        }
     }
 }
