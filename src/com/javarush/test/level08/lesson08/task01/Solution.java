f147b582e92f0014e2780682ca50e1be9097e6c8
bycter <bycter@gmail.com>
2017-01-04 23:27:34 +0300

    level08lesson08task01 processing

---
+++ src/com/javarush/test/level08/lesson08/task01/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level08.lesson08.task01;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* 20 слов на букву «Л»
+Создать множество строк (Set<String>), занести в него 20 слов на букву «Л».
+*/
+
+public class Solution
+{
+    public static HashSet<String> createSet()
+    {
+        Set<String> stringSet = new HashSet<>();
+
+    }
+}
