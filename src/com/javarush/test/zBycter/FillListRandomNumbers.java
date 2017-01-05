5fe7cd5aee65670b74575c0c0baf0e16f9a57666
Denis Yoshchyk <bycter@gmail.com>
2017-01-05 11:01:45 +0300

    Class FillListRandomNumbers ver. 1.1 - Remove variable 'arrayList', now this variable is local - Remove from constructors 'arrayList' initialization - Add 'arrayList' initialization to each method - Add method randomString() - generate string with random characters input: (String) lang,           (Integer) stringSize output: (String) - Add method fillArrayListRandStrings() - fill ArrayList by string with random characters input: (Integer) listSize,           (String) lang,           (Integer) stringSize output: (ArrayList<String>) - Remove method printArrayList() - i think now it not be useful method

--- src/com/javarush/test/zBycter/FillListRandomNumbers.java
+++ src/com/javarush/test/zBycter/FillListRandomNumbers.java
@@ -5,45 +5,72 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class FillListRandomNumbers {
 
-    private ArrayList<Integer> arrayList;
     private int minRandom, maxRandom;
     private final int minRandomDefault = 0, maxRandomDefault = 100;
 
     public FillListRandomNumbers() {
 
-        this.arrayList = new ArrayList<>();
         this.minRandom = this.minRandomDefault;
         this.maxRandom = this.maxRandomDefault;
     }
 
     public FillListRandomNumbers(int minRandom, int maxRandom) {
 
-        this.arrayList = new ArrayList<>();
         this.minRandom = minRandom;
         this.maxRandom = maxRandom;
     }
 
     public ArrayList<Integer> fillArrayListRandNum(Integer listSize) {
 
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
         for (int i = 0; i < listSize; i++) {
-            arrayList.add(ThreadLocalRandom.current().nextInt(minRandom, maxRandom));
+            integerArrayList.add(ThreadLocalRandom.current().nextInt(minRandom, maxRandom));
         }
-        return arrayList;
+        return integerArrayList;
     }
 
     public ArrayList<Integer> fillArrayListRandNum(Integer listSize, Integer min, Integer max) {
 
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
         for (int i = 0; i < listSize; i++) {
-            arrayList.add(ThreadLocalRandom.current().nextInt(min, max));
+            integerArrayList.add(ThreadLocalRandom.current().nextInt(min, max));
         }
-        return arrayList;
+        return integerArrayList;
     }
 
-    public void printArrayList() {
+    public ArrayList<String> fillArrayListRandStrings(String lang, Integer listSize, Integer stringSize) {
 
-        for (Integer integer : arrayList) {
-            System.out.println(integer);
+        ArrayList<String> stringArrayList = new ArrayList<>();
+
+        for (int i = 0; i < listSize; i++) {
+
+            stringArrayList.add(randomString(lang, stringSize));
         }
+        return stringArrayList;
+    }
+
+    /*
+        Метод используется для генерации строки заданной длины случайных символов.
+        Поддержка 2-х языков: английский, русский
+        Входные данные:
+        String lang - язык символов (eng(default), rus),
+        Integer stringSize - длина строки
+    */
+    public String randomString(String lang, Integer stringSize) {
+
+        String SALTCHARS_ENG = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String SALTCHARS_RUS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ1234567890";
+
+        String SALTCHARS = SALTCHARS_ENG;
+        if (lang.equals("rus")) SALTCHARS = SALTCHARS_RUS;
+
+        StringBuilder salt = new StringBuilder();
+        while (salt.length() < stringSize) {
+            float temp = ThreadLocalRandom.current().nextFloat();
+            int index = (int) (temp * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        return salt.toString();
     }
 
     public int getMinRandom() {
