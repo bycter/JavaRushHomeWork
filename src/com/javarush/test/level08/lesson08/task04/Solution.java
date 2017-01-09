ae8cffb761b3f201fb07911d5879b6201e5b0717
Denis Yoshchyk <bycter@gmail.com>
2017-01-09 15:32:35 +0300

    level08lesson08task04 processing

--- src/com/javarush/test/level08/lesson08/task04/Solution.java
+++ src/com/javarush/test/level08/lesson08/task04/Solution.java
@@ -1,7 +1,12 @@
 package com.javarush.test.level08.lesson08.task04;
 
+import javafx.scene.input.InputMethodTextRun;
+
+import java.time.Month;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 
 /* Удалить всех людей, родившихся летом
 Создать словарь (Map<String, Date>) и занести в него десять записей по принципу: «фамилия» - «дата рождения».
@@ -12,16 +17,26 @@ public class Solution
 {
     public static HashMap<String, Date> createMap()
     {
-        HashMap<String, Date> map = new HashMap<String, Date>();
+        HashMap<String, Date> map = new HashMap<>();
         map.put("Stallone", new Date("JUNE 1 1980"));
-
+        map.put("Denis", new Date("APRIL 241 1989"));
+        map.put("Vasia", new Date("JUNE 1 1980"));
+        map.put("Ann", new Date("DECEMBER 6 1989"));
+        map.put("Tanya", new Date("JANUARY 7 1990"));
+        map.put("Dima", new Date("MAY 14 1980"));
+        map.put("Valentina", new Date("MAY 18 1980"));
+        map.put("Boris", new Date("AUGUST 10 1980"));
+        map.put("Nina", new Date("JANUARY 8 1980"));
+        map.put("Masha", new Date("FEBRUARY 23 1980"));
         //напишите тут ваш код
 
     }
 
     public static void removeAllSummerPeople(HashMap<String, Date> map)
     {
-        //напишите тут ваш код
+        for (Date date : map.values()) {
+            date.compareTo(new Date(new Month("JUNE")));
+        }
 
     }
 }
