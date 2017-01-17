04259374485e56c7ce97d4e419e3abf3efbac548
Denis Yoshchyk <bycter@gmail.com>
2017-01-17 15:39:43 +0300

    level08lesson11home09 processing

--- src/com/javarush/test/level08/lesson11/home09/Solution.java
+++ src/com/javarush/test/level08/lesson11/home09/Solution.java
@@ -1,8 +1,6 @@
 package com.javarush.test.level08.lesson11.home09;
 
-import java.util.Date;
-
-/* Работа с датой
+import java.util.Date;/* Работа с датой
 1. Реализовать метод isDateOdd(String date) так, чтобы он возвращал true, если количество дней с начала года - нечетное число, иначе false
 2. String date передается в формате MAY 1 2013
 Не забудьте учесть первый день года.
@@ -11,14 +9,28 @@ JANUARY 1 2000 = true
 JANUARY 2 2020 = false
 */
 
-public class Solution
-{
-    public static void main(String[] args)
-    {
+public class Solution {
+
+    public static void main(String[] args) {
+
+//        System.out.println("JANUARY 1 2000 = " + isDateOdd("JANUARY 1 2000"));
+//        System.out.println("JANUARY 2 2020 = " + isDateOdd("JANUARY 2 2020"));
     }
 
-    public static boolean isDateOdd(String date)
-    {
-        return true;
+    public static boolean isDateOdd(String date) {
+
+        Date yearStartDay = new Date();
+        yearStartDay.setDate(0);
+        yearStartDay.setMonth(0);
+        yearStartDay.setHours(0);
+        yearStartDay.setMinutes(0);
+        yearStartDay.setSeconds(0);
+
+        Date date1 = new Date(date);
+        long msInOneDay = 24 * 60 * 60 * 1000;
+        int dayCount = (int) ((date1.getTime() - yearStartDay.getTime()) / msInOneDay);
+
+        if ((dayCount % 2) == 0) return false;
+        else return true;
     }
 }
