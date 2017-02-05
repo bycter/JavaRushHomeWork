1d5c2cab6a4fcf5c94d6f213b99f74b455a7a7ad
bycter <bycter@gmail.com>
2017-02-05 11:34:30 +0300

    level12lesson04task03 completed

---
+++ src/com/javarush/test/level12/lesson04/task03/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static void print(int input) {
+
+        System.out.println(input);
+    }
+
+    public static void print(Integer input) {
+
+        System.out.println(input);
+    }
+
+    public static void print(String input) {
+
+        System.out.println(input);
+    }
+
+    public static void print(double input) {
+
+        System.out.println(input);
+    }
+
+    public static void print(long input) {
+
+        System.out.println(input);
+    }
+}
